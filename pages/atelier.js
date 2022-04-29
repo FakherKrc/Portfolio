c894c0331abe5d07f3298e747854a71f66ea2319
FakherKrc <Fakher.karchid1@gmail.com>
2022-04-29 19:59:30 +0200

    third commit

--- pages/atelier.js
+++ pages/atelier.js
@@ -14,32 +14,3 @@ element.addEventListener("click", function(e){
 
 
 })
-
-function hover(event) {
-    
-       if(!event.currentTarget.firstChild.classList.contains("filled"))
-       {
-        event.currentTarget.firstChild.classList.add("filled")
-        event.currentTarget.lastChild.classList.add("afill");
-       }
-        
-    
-  }
-
-  function noHover(event) {
-    if(event.currentTarget.firstChild.classList.contains("filled"))
-       {
-        event.currentTarget.firstChild.classList.remove("filled")
-        event.currentTarget.lastChild.classList.remove("afill");
-        document.querySelector(".svgfill").classList.remove("filled");
-        document.querySelector(".links a").classList.remove("afill");
-    
-       }
-  }
-
-var table_link = document.getElementsByClassName("links");
-for(var i= 0; i<table_link.length; i++)
-{
-    table_link[i].addEventListener("mouseover", hover);
-    table_link[i].addEventListener("mouseout", noHover);
-}
---
+++ pages/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.example.mediatek86formations"},{"l":"com.example.mediatek86formations.controleur"},{"l":"com.example.mediatek86formations.modele"},{"l":"com.example.mediatek86formations.outils"},{"l":"com.example.mediatek86formations.vue"}]
